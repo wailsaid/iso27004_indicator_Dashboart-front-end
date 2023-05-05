859ac44b99cc0c785c352149a5ee7830944adc19
said_wail <waildebbaghi@gmail.com>
2023-05-05 23:40:34 +0100

    fist setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideBarComponent } from './component/side-bar/side-bar.component';
+import { TopBarComponent } from './component/top-bar/top-bar.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+/* import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'; */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    TopBarComponent,
+    DashboardComponent,
+
   ],
-  imports: [
+    imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
